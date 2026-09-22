6b533f8b64e98bcaa33fe287f1779730e12de81e
Jherrerado <jhan.joshua.h.g@gmail.com>
2026-09-22 17:20:12 -0400

    feat(db): agregar script SQL de registro de usuarios

---
+++ sql/Usuarios.sql
@@ -0,0 +1,22 @@
+-- Usuarios de la app
+
+--  correo                      | contraseña en texto plano (para probar el login)
+--  ---------------------------|--------------------------------------------------
+--  integrante1@jn3kicks.com   | integrante1
+--  integrante2@jn3kicks.com   | integrante2
+--  integrante3@jn3kicks.com   | integrante3
+--  integrante4@jn3kicks.com   | integrante4
+--  integrante5@jn3kicks.com   | integrante5
+--  integrante6@jn3kicks.com   | integrante6
+--  integrante7@jn3kicks.com   | integrante7
+--  integrante8@jn3kicks.com   | integrante8
+
+INSERT INTO usuarios (nombre, correo, contrasena) VALUES
+('Integrante 1', 'integrante1@jn3kicks.com', '$2y$10$eISH7I19A07dr2cGBTAcVetJYYxSN.a.wuncJfNePB0cc1lyusT0u'),
+('Integrante 2', 'integrante2@jn3kicks.com', '$2y$10$AQGI9STexysw0XTDTznRl.WrvHBRjLsAJsrKYiNTi59QwS7IOyqPu'),
+('Integrante 3', 'integrante3@jn3kicks.com', '$2y$10$KroIMrRHJpFAMBcwmD2ozex/tZbjeR.mtvopOwDq5zGu5EBCiMKwO'),
+('Integrante 4', 'integrante4@jn3kicks.com', '$2y$10$szuB.a1TGsW2HvsekG2tGO5wcz7s8wodniWljlVF4Rmxrd/732uoi'),
+('Integrante 5', 'integrante5@jn3kicks.com', '$2y$10$mGbWQLc0IaZ.xpDHMm4z5OJkjyxjBM.akQiQHB7vfeH5pRUm1Q0wy'),
+('Integrante 6', 'integrante6@jn3kicks.com', '$2y$10$Jwo9lFNy/k5d/iq8CTOb.uKFwj3WkNd91EqOo/I5geHK5Ap/8eDOW'),
+('Integrante 7', 'integrante7@jn3kicks.com', '$2y$10$.1GnGOBFEP.roL1wxZcjl.vUt5GDZvcGO1oJqJHbBEqiGlSUasnwK'),
+('Integrante 8', 'integrante8@jn3kicks.com', '$2y$10$qVzANfBx1V56YoBlh6d9eux6wE9m8oz0Gw1bSJ5uEc/84pgshP28a');
